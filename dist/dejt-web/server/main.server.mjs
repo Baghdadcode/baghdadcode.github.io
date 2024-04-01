c7feee6238ad391993c41a10abc092c49448d518
Baghdadcode <bam1891dad@gmail.com>
2024-04-02 00:35:04 +0200

    teas

--- dist/dejt-web/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-2OTTFMHD.mjs";import"./chunk-I4SNV7BV.mjs";import"./chunk-P5KR4CXA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ALDLVLTR.mjs";import"./chunk-TMSQAC6R.mjs";import"./chunk-RSAFAREB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
